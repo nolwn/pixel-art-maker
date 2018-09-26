fdccb1c6ec665d6ff9bfa81011cf788047982217
Nolan <nolan.hellyer@gmail.com>
2018-09-26 00:22:11 -0700

    basic monochrome click drawing added

--- scripts/pixel-art-maker.js
+++ scripts/pixel-art-maker.js
@@ -1,20 +1,18 @@
-var board;
-
 document.addEventListener("DOMContentLoaded", function(e){
   const artboard = document.querySelector("#artboard");
   const width = 20;
   const height = 20;
-  const defaultColor = "#ffffff";
 
-  let board = [];
+  letstate = {};
   let color = "black";
 
-  board = init(20, 20, "white");
-  paint(board, artboard);
+  state = init(20, 20, "white");
+  paint(state, artboard);
 });
 
 function init(width, height, defaultColor) {
   const rows = [];
+  const state = {}
   for (let i = 0; i < width; i++) {
     let fields = [];
     for (let j = 0; j < height; j++) {
@@ -22,17 +20,26 @@ function init(width, height, defaultColor) {
     }
     rows.push(fields);
   }
-  return rows;
+  state.board = rows;
+  state.brush = "#4400ff";
+  return state;
 }
 
-function destroy(board, artboard) {
+function destroy(artboard) {
   while (artboard.children.length) {
     artboard.removeChild(artboard.children[0]);
   }
 }
 
-function paint(board, artboard) {
-  destroy(board, artboard);
+function color(x, y, {board, brush}) {
+  console.log(x);
+  board[y][x] = brush;
+  const artboard = document.querySelector("#artboard");
+  paint(state, artboard);
+}
+
+function paint({board, brush}, artboard) {
+  destroy(artboard);
   for (let i = 0; i < board.length; i++) {
     let row = document.createElement("div");
     row.classList.add("row");
@@ -40,6 +47,13 @@ function paint(board, artboard) {
       let pixel = document.createElement("div");
       pixel.className = "pixel";
       pixel.style.background = board[i][j];
+      pixel.setAttribute("x", j);
+      pixel.setAttribute("y", i);
+      pixel.addEventListener("click", function(e) {
+        // console.log(this.getAttribute("x"), this.getAttribute("y"));
+        color(this.getAttribute("x"), this.getAttribute("y"), state);
+        // console.log(board, brush);
+      });
       row.appendChild(pixel);
     }
     artboard.appendChild(row);
